88804e8883abd2543e59b00cf84d22d3d40095b0
wnubhav <anubhav10b@gmail.com>
2025-03-08 12:11:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-document.getElementById('converterForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const amount = document.getElementById('amount').value;
-    const fromCurrency = document.getElementById('fromCurrency').value;
-    const toCurrency = document.getElementById('toCurrency').value;
-
-    fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
+setInterval(function() {
+    fetch('https://api.exchangerate-api.com/v4/latest/USD')
         .then(response => response.json())
         .then(data => {
-            const exchangeRate = data.rates[toCurrency];
-            const result = amount * exchangeRate;
-
-            document.getElementById('result').innerText = `${amount} ${fromCurrency} is equal to ${result.toFixed(2)} ${toCurrency}`;
+            // Update exchange rates in your app
         })
         .catch(error => console.error('Error:', error));
-});
+}, 60000); // Update every minute
